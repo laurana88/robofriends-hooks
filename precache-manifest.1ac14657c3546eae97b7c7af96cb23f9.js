42022da3d6cc61512c71ec6d30a597a5c8e10597
Laura Smith <laura.ann.smith88@gmail.com>
2021-07-01 16:42:23 -0400

    Updates

---
+++ precache-manifest.1ac14657c3546eae97b7c7af96cb23f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1161cce5292e00e32cfa32d41029c711",
+    "url": "/robofriends-hooks/index.html"
+  },
+  {
+    "revision": "6e898ac76df91a5328ba",
+    "url": "/robofriends-hooks/static/css/2.ac18de59.chunk.css"
+  },
+  {
+    "revision": "4ccf2f0c315fcb3d99e1",
+    "url": "/robofriends-hooks/static/css/main.559d9d54.chunk.css"
+  },
+  {
+    "revision": "6e898ac76df91a5328ba",
+    "url": "/robofriends-hooks/static/js/2.ac186811.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends-hooks/static/js/2.ac186811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccf2f0c315fcb3d99e1",
+    "url": "/robofriends-hooks/static/js/main.d96acf70.chunk.js"
+  },
+  {
+    "revision": "796225fb18ecd50a8de5",
+    "url": "/robofriends-hooks/static/js/runtime-main.aa616c01.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-hooks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
